0b0078b08d124bba9ea8fe1657941ea3c72c63d3
Rico Apon <rico.apon@gmail.com>
2022-10-28 19:41:46 +0200

    Implement HTMX to execute commands via backend

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,16 +1,32 @@
 package com.example.demo;
 
+import com.example.demo.frontend.Line;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.Date;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class HomeController {
     @RequestMapping("/")
-    public String home(Model model) {
-        model.addAttribute("serverTime", new Date());
+    public String home() {
         return "index";
+    }
+
+    @RequestMapping("/post-command")
+    public String postCommand(HttpServletResponse response, Model model, @RequestParam("command") String command) {
+        // Set header for fun.
+        if (command.equals("donotclear")) {
+            response.addHeader("Terminal-Clear-Input", "false");
+        }
+
+        Line line = new Line();
+        line.command = command;
+        line.location = "tutorial";
+        line.response = "I have not implemented this command yet!";
+        model.addAttribute("line", line);
+        return "executed-command";
     }
 }
